cd081cf659cae602ee0984a7e656fd587cfc1296
Jeremy Johnstone <github@jeremyjohnstone.com>
2010-05-18 17:57:37 -0700

    Adding in debug console

--- webroot/chat.js
+++ webroot/chat.js
@@ -7,16 +7,24 @@ var chat = function() {
 	var clientId = Math.random();
 	var latestId = -1;
 	
+	function debugMessage(message) {
+		var markup = "<div>" + message + "</div>";
+		debugRef.innerHTML += markup;
+		debugRef.scrollTop = debugRef.scrollHeight;
+	}
 	
-	function init() {		
+	function init() {
 		submitRef = document.getElementById("sendBtn");
 		entryRef = document.getElementById("entry");
 		chatRef = document.getElementById("chatMessages");
 		debugRef = document.getElementById("debug");
+		debugMessage("Initialized!");
 	}
 	
 	function sendHandler() {
 		var message = entryRef.value;
+		
+		debugMessage("Sending message");
 		
 		submitRef.value = "Send...";
 		submitRef.disabled = true;
@@ -25,9 +33,11 @@ var chat = function() {
 		var node = document.createElement("script");
 		node.src = "/api/send/?clientId=" + clientId + "&content=" + message + "&rand=" + Math.random();
 		document.body.appendChild(node);
+		debugMessage("Script node appended");
 	}
 	
 	function bindHandlers() {
+		debugMessage("Binding handlers");
 		submitRef.onclick = sendHandler;
 		entryRef.onkeyup = function(e) {
 			if(e.keyCode == 13) {
@@ -37,6 +47,7 @@ var chat = function() {
 	}
 	
 	function processMessages(jsonString) {
+		debugMessage("Processing messages");
 		var messages = json_parse(jsonString).response;
 		for(var i=0; i < messages.length; i++) {
 			var message = messages[i];
@@ -46,22 +57,26 @@ var chat = function() {
 			latestId = message.sequence;
 		}
 		chatRef.scrollTop = chatRef.scrollHeight;
+		debugMessage("Finished processing messages");
 	}
 	
 	function handleXHRResponse() {
 		if(this.readyState != 4) { return; }
 		if(this.status == 200) {
+			debugMessage("Got 200 response"); 
 			processMessages(this.responseText);
 			startPuller();
 		} else {
+			debugMessage("Error or timeout, restarting");
 			startPuller();
 		}
 	}
 	
 	function startPuller() {
+		debugMessage("Starting new XHR"); 
 		var xhr = new XMLHttpRequest();
 		xhr.onreadystatechange = handleXHRResponse;
-		xhr.open("GET", "/api/poll/" + latestId);
+		xhr.open("GET", "/api/poll/" + latestId, true);
 		xhr.send();
 	}
 	
@@ -73,7 +88,13 @@ var chat = function() {
 		},
 		receiveSendResponse: function(response) {
 			if(response.status == 200) {
+				debugMessage("Message sent successfully"); 
 				entryRef.value = "";
+				entryRef.disabled = false;
+				submitRef.value = "Send";
+				submitRef.disabled = false;
+			} else {
+				debugMessage("Message sending failed"); 
 				entryRef.disabled = false;
 				submitRef.value = "Send";
 				submitRef.disabled = false;
